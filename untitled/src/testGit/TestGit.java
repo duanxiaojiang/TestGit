33b90607bbad6084360c30dc8e8b3e0e68d423b6
duanxiaojiang <182416700@qq.com>
2016-01-05 19:27:30 +0800

    Initial commit

---
+++ untitled/src/testGit/TestGit.java
@@ -0,0 +1,10 @@
+package testGit;
+
+/**
+ * Created by duansu on 2016/1/5.
+ */
+public class TestGit {
+    public static void main(String [] args){
+        System.out.println("这个程序用于测试将程序上传到github并更新！");
+    }
+}
